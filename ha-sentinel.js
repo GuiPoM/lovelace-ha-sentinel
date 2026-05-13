695a60f692522d31823aa7bb5885c569bf76280f
GuiPoM <11942518+GuiPoM@users.noreply.github.com>
2026-05-13 19:37:12 +0200

    fix: apps summary icon mdi:puzzle-off does not exist, use mdi:puzzle

--- ha-sentinel.js
+++ ha-sentinel.js
@@ -470,7 +470,7 @@ class HaSentinelAppsCard extends HTMLElement {
 
     const summaryRow = `
       <div class="row">
-        <ha-icon icon="${problemCount > 0 ? "mdi:puzzle-remove" : "mdi:puzzle-off"}"
+        <ha-icon icon="${problemCount > 0 ? "mdi:puzzle-remove" : "mdi:puzzle"}"
           style="color:${problemCount > 0 ? COLOR.error : COLOR.ok}"></ha-icon>
         <div class="info">
           <span class="name">${L.apps_error}</span>
